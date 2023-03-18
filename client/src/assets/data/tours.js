6b52b0093b46d588d4e224d4d730943ff0a8481a
Gleb Katsko <BelgOkstak@gmail.com>
2023-03-18 14:28:24 +0300

    defined schemas user schema defined, review and tour

---
+++ client/src/assets/data/tours.js
@@ -0,0 +1,156 @@
+import tourImg01 from "./tour-img01.jpg";
+import tourImg02 from "./tour-img02.jpg";
+import tourImg03 from "./tour-img03.jpg";
+import tourImg04 from "./tour-img04.jpg";
+import tourImg05 from "./tour-img05.jpg";
+import tourImg06 from "./tour-img06.jpg";
+import tourImg07 from "./tour-img07.jpg";
+
+const tours = [
+  {
+    id: "01",
+    title: "Westminister Bridge",
+    city: "London",
+    distance: 300,
+    price: 99,
+    maxGroupSize: 10,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg01,
+    featured: true,
+  },
+  {
+    id: "02",
+    title: "Bali, Indonesia",
+    city: "Indonesia",
+    distance: 400,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg02,
+    featured: true,
+  },
+  {
+    id: "03",
+    title: "Snowy Mountains, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg03,
+    featured: true,
+  },
+  {
+    id: "04",
+    title: "Beautiful Sunrise, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg04,
+    featured: true,
+  },
+  {
+    id: "05",
+    title: "Nusa Pendia Bali, Indonesia",
+    city: "Indonesia",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg05,
+    featured: false,
+  },
+  {
+    id: "06",
+    title: "Cherry Blossoms Spring",
+    city: "Japan",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg06,
+    featured: false,
+  },
+  {
+    id: "07",
+    title: "Holmen Lofoten",
+    city: "France",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg07,
+    featured: false,
+  },
+  {
+    id: "08",
+    title: "Snowy Mountains, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg03,
+    featured: false,
+  },
+];
+
+export default tours;
