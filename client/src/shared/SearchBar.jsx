eef9aa3b05e5e9284dd750cd43e451d758c73561
Gleb Katsko <BelgOkstak@gmail.com>
2023-04-17 21:40:11 +0300

    fixed search bar added dispatch calendar and back calendar, and locations from where and where

--- client/src/shared/SearchBar.jsx
+++ client/src/shared/SearchBar.jsx
@@ -5,15 +5,15 @@ import {Col, Form, FormGroup} from "reactstrap";
 const SearchBar = () => {
 
     const locationRef = useRef('')
-    const distanceRef = useRef(0)
+    const dateRef = useRef(0)
     const maxGroupSizeRef = useRef(0)
 
     const searchHandler = () => {
         const location = locationRef.current.value
-        const distance = distanceRef.current.value
+        const date = dateRef.current.value
         const maxGroupSize = maxGroupSizeRef.current.value
 
-        if(location === ''|| distance === '' || maxGroupSize === ''){
+        if(location === ''|| date === '' || maxGroupSize === ''){
             return alert ('All fields are required!')
         }
     }
@@ -22,23 +22,42 @@ const SearchBar = () => {
     return (
         <Col lg = '12'>
             <div className="search__bar">
-                <Form className = "d-flex align-items-center gap-4">
+                <Form className = "d-flex align-items-center gap-3">
                     <FormGroup className="d-flex gap-3 form__group form__group-fast">
                         <span>
                             <i className= "ri-map-pin-line"></i>
                         </span>
                         <div>
-                            <h6>Локация</h6>
-                            <input type = "text" placeholder="Куда отправимся?" ref={locationRef}/>
+                            <input type = "text" placeholder="Откуда?" ref={locationRef}/>
                         </div>
                     </FormGroup>
                     <FormGroup className="d-flex gap-3 form__group form__group-fast">
                         <span>
-                            <i className= "ri-map-pin-time-line"></i>
+                            <i className= "ri-map-pin-fill"></i>
                         </span>
                         <div>
-                            <h6>Расстояние</h6>
-                            <input type = "number" placeholder="Расстояние k/m" ref={distanceRef}/>
+                            <input type = "text" placeholder="Куда?" ref={locationRef}/>
+                        </div>
+                    </FormGroup>
+                    <FormGroup className="d-flex gap-3 form__group form__group-fast">
+                        <span>
+                            <i className= "ri-calendar-line"></i>
+                        </span>
+                        <div>
+                            <input type="text" placeholder="Когда" ref={dateRef}
+                                   onFocus={(e) => e.target.type = 'date'}
+                                   onBlur={(e) => e.target.type = 'text'}/>
+                        </div>
+                    </FormGroup>
+                    <FormGroup className="d-flex gap-3 form__group form__group-fast">
+                        <span>
+                            <i className="ri-calendar-fill"></i>
+                        </span>
+                        <div>
+                            <input type="text" placeholder="Обратно" ref={dateRef}
+                                   onFocus={(e) => e.target.type = 'date'}
+                                   onBlur={(e) => e.target.type = 'text'}
+                                   defaultChecked="Обратно" />
                         </div>
                     </FormGroup>
                     <FormGroup className="d-flex gap-3 form__group form__group-last">
@@ -46,7 +65,6 @@ const SearchBar = () => {
                             <i className= "ri-group-line"></i>
                         </span>
                         <div>
-                            <h6>Людей</h6>
                             <input type = "number" placeholder="0" ref={maxGroupSizeRef}/>
                         </div>
                     </FormGroup>
