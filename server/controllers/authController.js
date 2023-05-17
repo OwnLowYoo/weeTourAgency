09c38a5247d28bba3a8d8eaa4f10e35e920e6f9c
Gleb Katsko <BelgOkstak@gmail.com>
2023-05-17 12:29:58 +0300

    login with jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,7 +25,43 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+
+    const email = req.body.email
+
     try {
-    }catch (err) {}
+
+        const user = await User.findOne({email})
+
+        //if user doesn't exist
+        if(!user){
+            return res.status(404).json({success:false, message:'User not found'})
+        }
+
+        //if user is exist then check the password or compare the password
+        const checkCorrectPassword = bcrypt.compare(req.body.password, user.password)
+
+        //if password is incorrect
+        if(!checkCorrectPassword){
+            return  res.status(401).
+            json({success:false, message:'Incorrect email or password'})
+        }
+
+        const {password, role, ...rest} = user._doc
+
+        //create jwt token
+        const token = jwt.sign(
+            {id:user._id, role:user.role},
+            process.env.JWT_SECRET_KEY, {expiresIn: "15d"}
+    );
+
+        // set token in the browser coolies and send the response to the client
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            expires: token.expiresIn
+        }).status(200).json({success: true, message:"Successfully login", data:{...rest}})
+    }catch (err) {
+        res.status(500).
+        json({success:false, message:'Failed to login'})
+    }
 };
 
