e74531eccb24d84073b64f2b1cef8b9a3cbbb84a
Gleb Katsko <BelgOkstak@gmail.com>
2023-05-24 15:58:40 +0300

    connected database to featured tours

--- client/src/components/Featured-tours/FeaturedTourList.jsx
+++ client/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import TourCard from "../../shared/TourCard";
-import tourData from "../../assets/data/tours"
 import {Col} from "reactstrap";
+
+import useFetch from "../../hooks/useFetch";
+import {BASE_URL} from "../../utils/config";
+
 const FeaturedTourList = () => {
+
+    const {data: featuredTours} = useFetch(
+        `${BASE_URL}/tours/search/getFeaturedTours`
+    );
+
+    console.log(featuredTours)
+
     return (
         <>
-            {tourData?.map(tour=> (
-                <Col lg = "3" className="mb-4" key={tour.id}>
+            {featuredTours ?.map(tour=> (
+                <Col lg = "3" className="mb-4" key={tour._id}>
                     <TourCard tour = {tour} />
                 </Col>
             ))}
