758f0b8cc54c1b8e4f61f63c2e341f0d8347a61a
Gleb Katsko <BelgOkstak@gmail.com>
2023-05-27 17:46:01 +0300

    loading for featured tours

--- client/src/components/Featured-tours/FeaturedTourList.jsx
+++ client/src/components/Featured-tours/FeaturedTourList.jsx
@@ -7,15 +7,19 @@ import {BASE_URL} from "../../utils/config";
 
 const FeaturedTourList = () => {
 
-    const {data: featuredTours} = useFetch(
+    const {data: featuredTours, loading, error} = useFetch(
         `${BASE_URL}/tours/search/getFeaturedTours`
     );
 
-    console.log(featuredTours)
+    console.log(featuredTours);
 
     return (
         <>
-            {featuredTours ?.map(tour=> (
+            {loading && <h4>Loading............</h4>}
+            {error && !error && <h4>{error}</h4>}
+            {!loading &&
+                !error &&
+            featuredTours?.map(tour=> (
                 <Col lg = "3" className="mb-4" key={tour._id}>
                     <TourCard tour = {tour} />
                 </Col>
