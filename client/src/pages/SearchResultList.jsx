a5c978009757cc4fdc302b94b66435292e424dff
Gleb Katsko <BelgOkstak@gmail.com>
2023-06-04 16:37:39 +0300

    search result list

--- client/src/pages/SearchResultList.jsx
+++ client/src/pages/SearchResultList.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from "react";
+
+import CommonSection from "../shared/CommonSection";
+import { Container, Row, Col} from "reactstrap";
+
+import { useLocation } from "react-router-dom";
+import TourCard from "../shared/TourCard";
 
 const SearchResultList = () => {
-    return (
-        <div>
-            SearchResultList
-        </div>
-    );
-};
 
-export default SearchResultList;
+    const  location = useLocation()
+
+    const [data] = useState(location.state)
+
+    console.log(data)
+
+    return (
+        <>
+        <CommonSection title = {"Tour Search Result"} />
+            <section>
+                <Container>
+
+                </Container>
+            </section>
+        </>
+    )
+}
+
+export default SearchResultList
